5e5715cd4e4c84c1e1a09a993f6440f65ce5ed47
Srikur <skanupa@gmail.com>
2017-05-20 19:31:22 -0400

    Added basic maps functionality and updated some other stuff

--- app/src/main/java/srikurk/jcpsbusfinder/MainActivity.java
+++ app/src/main/java/srikurk/jcpsbusfinder/MainActivity.java
@@ -1,20 +1,20 @@
 /*
 *   Created by Srikur Kanuparthy and Adithya Iyengar
 *   Copyright 2017 All Rights Reserved
-* */
+*/
 
 package srikurk.jcpsbusfinder;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.view.View;
-import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -81,17 +81,18 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_bus) {
-            // Handle the camera action
+            // Open my buses
         } else if (id == R.id.nav_map) {
+            //Open maps
 
         } else if (id == R.id.nav_school) {
-
+            //Open my schools
         } else if (id == R.id.nav_stop) {
-
+            //Open my stops
         } else if (id == R.id.nav_settings) {
-
+            //Open settings
         } else if (id == R.id.nav_logout) {
-
+            //Logout
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
