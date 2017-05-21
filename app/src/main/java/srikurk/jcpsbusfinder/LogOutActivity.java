7e2b74a48c55f8ce981969bd36b01fcf10fd12b5
Adithya Iyengar <adithya_iyengar@hotmail.com>
2017-05-21 17:04:42 -0400

    when you press the logout button on menu bar, it goes to the login screen

--- app/src/main/java/srikurk/jcpsbusfinder/LogOutActivity.java
+++ app/src/main/java/srikurk/jcpsbusfinder/LogOutActivity.java
@@ -3,11 +3,13 @@ package srikurk.jcpsbusfinder;
 /**
  * Created by adithya on 5/21/2017.
  */
+import android.content.BroadcastReceiver;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.content.IntentFilter;
 
 public class LogOutActivity extends AppCompatActivity {
 
@@ -16,4 +18,8 @@ public class LogOutActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+    public void changeLayout(View view){
+        setContentView(R.layout.activity_login);
+    }
 }
+
