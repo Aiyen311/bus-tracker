2baba662c0c1d1c58675fd02bf4f37d0173c57bf
Adithya Iyengar <adithya_iyengar@hotmail.com>
2017-05-21 10:41:35 -0400

    fixed toolbar color

--- app/src/main/java/srikurk/jcpsbusfinder/LogOutActivity.java
+++ app/src/main/java/srikurk/jcpsbusfinder/LogOutActivity.java
@@ -10,6 +10,8 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class LogOutActivity extends AppCompatActivity {
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
