e8591eda7121b4ca83e24c2849661af21cd0d13b
Adithya Iyengar <adithya_iyengar@hotmail.com>
2017-05-20 23:19:38 -0400

    look at the manifest and a line in the loginactivity

---
+++ app/src/main/java/srikurk/jcpsbusfinder/SignupActivity.java
@@ -0,0 +1,136 @@
+package srikurk.jcpsbusfinder;
+
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+/**
+ * Created by adithya on 5/20/2017.
+ */
+
+public class SignupActivity extends AppCompatActivity {
+private static final String TAG = "SignupActivity";
+
+@InjectView(R.id.input_name) EditText _nameText;
+@InjectView(R.id.input_email1) EditText _emailText1;
+@InjectView(R.id.input_email2) EditText _emailText2;
+@InjectView(R.id.input_password) EditText _passwordText;
+@InjectView(R.id.btn_signup) Button _signupButton;
+@InjectView(R.id.link_login) TextView _loginLink;
+
+@Override
+public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+        ButterKnife.inject(this);
+
+        _signupButton.setOnClickListener(new View.OnClickListener() {
+@Override
+public void onClick(View v) {
+        signup();
+        }
+        });
+
+        _loginLink.setOnClickListener(new View.OnClickListener() {
+@Override
+public void onClick(View v) {
+        // Finish the registration screen and return to the Login activity
+        finish();
+        }
+        });
+        }
+
+public void signup() {
+        Log.d(TAG, "Signup");
+
+        if (!validate()) {
+        onSignupFailed();
+        return;
+        }
+
+        _signupButton.setEnabled(false);
+
+final ProgressDialog progressDialog = new ProgressDialog(SignupActivity.this,
+        R.style.AppTheme_Dark_Dialog);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Creating Account...");
+        progressDialog.show();
+
+        String name = _nameText.getText().toString();
+        String email1 = _emailText1.getText().toString();
+        String email2 = _emailText2.getText().toString();
+        String password = _passwordText.getText().toString();
+
+        new android.os.Handler().postDelayed(
+        new Runnable() {
+public void run() {
+        // On complete call either onSignupSuccess or onSignupFailed
+        // depending on success
+        onSignupSuccess();
+        // onSignupFailed();
+        progressDialog.dismiss();
+        }
+        }, 3000);
+        }
+
+
+public void onSignupSuccess() {
+        _signupButton.setEnabled(true);
+        setResult(RESULT_OK, null);
+        finish();
+        }
+
+public void onSignupFailed() {
+        Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
+
+        _signupButton.setEnabled(true);
+        }
+
+public boolean validate() {
+        boolean valid = true;
+
+        String name = _nameText.getText().toString();
+        String email1 = _emailText1.getText().toString();
+        String email2 = _emailText2.getText().toString();
+        String password = _passwordText.getText().toString();
+
+        if (name.isEmpty() || name.length() < 3) {
+        _nameText.setError("Please enter at least 3 characters");
+        valid = false;
+        } else {
+        _nameText.setError(null);
+        }
+
+        if (email1.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email1).matches()) {
+        _emailText1.setError("Please enter a valid email address");
+        valid = false;
+        } else {
+        _emailText1.setError(null);
+        }
+
+        if (email2.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email2).matches() || email1 != email2){
+        _emailText2.setError("Please make sure both emails match each other");
+        valid = false;
+        } else {
+        _emailText2.setError(null);
+        }
+
+        if (password.isEmpty() || password.length() < 8 || password.length() > 15) {
+        _passwordText.setError("Please enter between 8 and 15 alphanumeric characters");
+        valid = false;
+        } else {
+        _passwordText.setError(null);
+        }
+
+        return valid;
+        }
+    }
